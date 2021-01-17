906652438b82c943064197b6765b22863b414e05
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-17 15:40:42 -0300

    listo el desafío

--- app/controllers/ufvaluerequests_controller.rb
+++ app/controllers/ufvaluerequests_controller.rb
@@ -38,6 +38,44 @@ class UfvaluerequestsController < ApplicationController
     @ufvaluerequest.destroy
   end
 
+    #bring the UF value
+    def uf
+      api_url = "https://mindicador.cl/api/uf/#{(params[:date])}"
+      response = HTTParty.get(api_url)
+      responsetohash = JSON.parse(response.read_body)
+      if responsetohash['serie'][0].nil?
+        return render json: {mensaje:"Valor no existe en esa fecha"}
+      else 
+        if request.headers['X-CLIENT'].present?
+          Ufvaluerequest.create(client: request.headers['X-CLIENT'], requestedDate: "#{params[:date]}", ufvalue: responsetohash['serie'][0]['valor'] )
+          render json: responsetohash['serie'][0]['valor']
+        else
+          return render json: {mensaje:"falta colocar key = X-CLIENT y en Header su nombre de cliente"}
+        end
+      end
+    end
+
+    #Bring my requests count and detail
+    def my_requests
+      name = params[:name]
+      detail = []
+      (Ufvaluerequest.where(client: name)).each do |consult|
+        hash1 = {}
+        hash1[:requestedDate] = consult.requestedDate
+        hash1[:ufvalue] = consult.ufvalue
+        hash1[:created_at] = consult.created_at
+        detail.push(hash1)
+      end
+      hash = {"Cantidad de consultas": "#{Ufvaluerequest.where(client: name).count}", "Detalle de consultas": "#{detail}"}
+      render json: hash
+    end
+
+
+
+
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_ufvaluerequest
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :ufvaluerequests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/uf/:date", to: 'ufvaluerequests#uf'
+  get "/client/:name", to: 'ufvaluerequests#my_requests'
 end
